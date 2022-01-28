b3682c1795ad2da1a644691c98666e4458345e37
jrmillz <jonathan.robinson508@gmail.com>
2022-01-27 20:35:33 -0500

    working on  trying to get the button to work

--- sandbox.java
+++ sandbox.java
@@ -1,26 +1,25 @@
-package Extra;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Random;
 
-public class sandbox {
-    public static void main(String[] args) {        
+public class Sandbox {
+    public void insultGenerated() {        
 
-            String path = "C:/Users/Jon/Desktop/Jons Stuff/Java_SE/list.csv";
-            String line = "";           
+            String path = "C:/Users/Jon/Desktop/Random_Insult/list.csv";
+            String line = "";                      
 
             try {
                 BufferedReader br = new BufferedReader(new FileReader(path));
-                
+                                
                 while((line = br.readLine()) != null) {
-                    String[] values = line.split(",");
-                    
+                    String[] values = line.split(",");                    
                     Random r = new Random();
                     int randomIndex = r.nextInt(values.length);
-                    System.out.println(values[randomIndex]);
-            }
+                    String insult = (values[randomIndex]); 
+                    System.out.println(insult);                                       
+                }
 
             } catch (FileNotFoundException e) {
                 e.printStackTrace();            
@@ -28,5 +27,5 @@ public class sandbox {
                 e.printStackTrace();
             }
     
-    }
+        }        
 }
